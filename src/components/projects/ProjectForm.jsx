8a9bc06e678e509300a9f8c519e65d8692021511
Nikolas Agrafiotis <nagrafiotis@gmail.com>
2026-01-21 07:44:51 +0000

    File changes

--- src/components/projects/ProjectForm.jsx
+++ src/components/projects/ProjectForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import {
