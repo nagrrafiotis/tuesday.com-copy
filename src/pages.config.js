43fea29e1410ac244c6be1a30742bdcacdb8932b
Nikolas Agrafiotis <nagrafiotis@gmail.com>
2026-01-21 08:18:57 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,18 +1,17 @@
 import Analytics from './pages/Analytics';
 import Board from './pages/Board';
 import Boards from './pages/Boards';
+import ConstructionNotebook from './pages/ConstructionNotebook';
 import Contacts from './pages/Contacts';
 import Dashboard from './pages/Dashboard';
 import Expenses from './pages/Expenses';
 import FinancialOverview from './pages/FinancialOverview';
 import Gantt from './pages/Gantt';
-import Gmail from './pages/Gmail';
 import Home from './pages/Home';
 import Income from './pages/Income';
 import ProjectDetails from './pages/ProjectDetails';
 import Projects from './pages/Projects';
 import Tasks from './pages/Tasks';
-import ConstructionNotebook from './pages/ConstructionNotebook';
 import __Layout from './Layout.jsx';
 
 
@@ -20,18 +19,17 @@ export const PAGES = {
     "Analytics": Analytics,
     "Board": Board,
     "Boards": Boards,
+    "ConstructionNotebook": ConstructionNotebook,
     "Contacts": Contacts,
     "Dashboard": Dashboard,
     "Expenses": Expenses,
     "FinancialOverview": FinancialOverview,
     "Gantt": Gantt,
-    "Gmail": Gmail,
     "Home": Home,
     "Income": Income,
     "ProjectDetails": ProjectDetails,
     "Projects": Projects,
     "Tasks": Tasks,
-    "ConstructionNotebook": ConstructionNotebook,
 }
 
 export const pagesConfig = {
